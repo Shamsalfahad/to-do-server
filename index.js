be2f8ec691034d4ae911e6f166d98cf86051f003
Shamsalfahad <shamsalfahad3@gmail.com>
2022-07-01 10:37:41 +0600

    first commit

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// mongo
+
 const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.up5jq.mongodb.net:27017,cluster0-shard-00-01.up5jq.mongodb.net:27017,cluster0-shard-00-02.up5jq.mongodb.net:27017/?ssl=true&replicaSet=atlas-w5x22v-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
